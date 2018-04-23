f0c6556ab8e7c49ceeb46d8fc42609304d81f1f4
Jenkins-LST <p.brain@gmx.de>
2018-04-23 16:29:41 +0200

    Update Eigene.script.user.js

--- Eigene.script.user.js
+++ Eigene.script.user.js
@@ -14,7 +14,8 @@ function killElement(element) {
   }
 }
 var j = 10000; // so oft überprüfen eigene KHs
-var l = 50; // nicht weiter als 'l' km 
+var l = 50; // nicht weiter als 'l' km
+var n = 5; // nicht näher als 'n' km
 for (x = 1; x <= j; x++)
 {
   var fachabteilungOwn = 'table.table:nth-child(3) > tbody:nth-child(2) > tr:nth-child(' + x + ') > td:nth-child(4) > span:nth-child(1) ';
@@ -23,22 +24,29 @@ for (x = 1; x <= j; x++)
   var entfernungOwn = entfernOwn.split(',');
   var voOwn = 'table.table:nth-child(3) > tbody:nth-child(2) > tr:nth-child(' + x + ') > td:nth-child(5) > a:nth-child(1)';
   var vollOwn = document.querySelector(voOwn);
+  var mission = 'div.row:nth-child(6) > div:nth-child(2) > a:nth-child(1)';
   if (document.querySelector(fachabteilungOwn).innerHTML == 'Nein')
   {
     var kill = 'table.table:nth-child(3) > tbody:nth-child(2) > tr:nth-child(' + x + ')';
     killElement(document.querySelector(kill));
     x = x - 1;
-  } 
+  }
   else if (entfernungOwn[0] > l)
   {
     var kill = 'table.table:nth-child(3) > tbody:nth-child(2) > tr:nth-child(' + x + ')';
     killElement(document.querySelector(kill));
     x = x - 1;
-  } 
+  }
   else if (vollOwn.className == 'btn btn-danger')
   {
     var kill = 'table.table:nth-child(3) > tbody:nth-child(2) > tr:nth-child(' + x + ')';
     killElement(document.querySelector(kill));
     x = x - 1;
   }
-}
+    else if (document.querySelector(mission).innerHTML == 'Großfeuer im Krankenhaus' && entfernungOwn[0] < n)
+  {
+    var kill = 'table.table:nth-child(3) > tbody:nth-child(2) > tr:nth-child(' + x + ')';
+    killElement(document.querySelector(kill));
+    x = x - 1;
+    }
+  }
